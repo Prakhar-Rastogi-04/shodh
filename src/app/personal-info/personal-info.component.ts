270d261ee6a7e528e1596e53072de30099e619f6
Prakhar Rastogi <Prakhar.Rastogi@blazeclan.in>
2018-05-27 14:56:37 +0530

    change in json

--- src/app/personal-info/personal-info.component.ts
+++ src/app/personal-info/personal-info.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-personal-info',
   templateUrl: './personal-info.component.html',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+    this.callAllInfo();
+    console.log('ok');
+  }
+  callAllInfo = function () {
+    alert ('func called!');
+    this.http.get('https://raw.githubusercontent.com/Prakhar-Rastogi-04/shodh/master/my-personal-details.json')
+    .subscribe(
+      (data:any[]) => {
+        console.log(data);
+        this.personalData = data;
+        console.log(this.personalData.formProperties);
+      }
+    )
   }
 
 }
